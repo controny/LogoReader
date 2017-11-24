31b24859e0cd682b44d89dcddad2fbfc15252e1b
ControlDy <791219648@qq.com>
2017-11-24 09:33:56 +0000

    use a for loop in .sh

---
+++ slim_test/slim/train_and_eval.sh
@@ -0,0 +1,55 @@
+# Where the pre-trained Inception Resnet V2 checkpoint is saved to.
+PRETRAINED_CHECKPOINT_DIR=/root/Desktop/LogoReader/data/slim_test/checkpoints
+
+# Where the pre-trained Inception Resnet V2 checkpoint is saved to.
+MODEL_NAME=inception_resnet_v2
+
+# Where the training (fine-tuned) checkpoint and logs will be saved to.
+TRAIN_DIR=/root/Desktop/LogoReader/data/slim_test/training/${MODEL_NAME}
+
+# Where the dataset is saved to.
+DATASET_DIR=/root/Desktop/MX_down/Binarization_CarLogo51
+
+for i in {1..40}  
+do  
+	echo "$i th round"
+    python my_train_image_classifier.py \
+      --train_dir=${TRAIN_DIR} \
+      --dataset_name=carlogos51 \
+      --dataset_split_name=train \
+      --dataset_dir=${DATASET_DIR} \
+      --model_name=${MODEL_NAME} \
+      --checkpoint_path=${PRETRAINED_CHECKPOINT_DIR}/${MODEL_NAME}.ckpt \
+      --checkpoint_exclude_scopes=InceptionResnetV2/Logits,InceptionResnetV2/AuxLogits \
+      --trainable_scopes=InceptionResnetV2/Logits,InceptionResnetV2/AuxLogits \
+      --max_number_of_steps=100 \
+      --batch_size=100 \
+      --learning_rate=0.8 \
+      --learning_rate_decay_type=exponential \
+	  --num_epochs_per_decay=50 \
+      --save_interval_secs=300 \
+      --save_summaries_secs=300 \
+      --log_every_n_steps=100 \
+      --optimizer=adagrad \
+      --weight_decay=0.00004
+    
+    # Run evaluation with validation dataset.
+    python my_eval_image_classifier.py \
+      --checkpoint_path=${TRAIN_DIR} \
+      --eval_dir=${TRAIN_DIR} \
+      --dataset_name=carlogos51 \
+      --dataset_split_name=validation \
+      --dataset_dir=${DATASET_DIR} \
+      --model_name=${MODEL_NAME}
+
+done 
+# Run evaluation with test dataset.
+python my_eval_image_classifier.py \
+      --checkpoint_path=${TRAIN_DIR} \
+      --eval_dir=${TRAIN_DIR} \
+      --dataset_name=carlogos51 \
+      --dataset_split_name=test \
+      --dataset_dir=${DATASET_DIR} \
+      --model_name=${MODEL_NAME}
+
+
